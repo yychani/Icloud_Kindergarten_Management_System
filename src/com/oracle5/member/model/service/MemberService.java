eccfd498a88bb6b7e1ebcfa0f23c7c7724785355
wonky <wonky@DESKTOP-FJV5MK3>
2019-12-17 21:35:49 +0900

    선생님 주민번호 비었을 때 입력 받는거 추가

--- src/com/oracle5/member/model/service/MemberService.java
+++ src/com/oracle5/member/model/service/MemberService.java
@@ -1198,4 +1198,25 @@ public class MemberService {
 
 	}
 
+	public int updateTeacherInfo(int mno, Teacher t, Member m) {
+		Connection con = getConnection();
+		
+		int result = 0;
+		
+		int result1 = new MemberDao().updateTeacherInfo(con, mno, t);
+		
+		int result2 = new MemberDao().updateTeacherMInfo(con, mno, m);
+		
+		if(result1 > 0 && result2 > 0) {
+			commit(con);
+			result = 1;
+		} else {
+			rollback(con);
+		}
+		
+		close(con);
+		
+		return result;
+	}
+
 }
